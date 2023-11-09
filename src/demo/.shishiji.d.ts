f1933074ffaddd2a91c1abfc13de460f2386230f
KANOKIw <KUMANKENw@gmail.com>
2023-11-09 23:55:12 +0900

    tsc miss

---
+++ src/demo/.shishiji.d.ts
@@ -0,0 +1,16 @@
+interface Position {
+    0: number | null,
+    1: number | null,
+}
+
+
+interface Moved {
+    left: number,
+    top: number,
+}
+
+
+interface Coordinate {
+    x: number,
+    y: number,
+}
---
+++ src/demo/propaties.ts
@@ -0,0 +1,4 @@
+var lastTouchPos: Position = [ null, null ];
+var lastTouchesDis = 0;
+var lastCursorPos: Position = [ null, null ];
+var zoomratio = 10;
