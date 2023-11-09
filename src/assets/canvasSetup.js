5b905d026bb5b44276a3d9a3bc71a887a1cc06b9
KANOKIw <KUMANKENw@gmail.com>
2023-11-09 23:40:55 +0900

    added touch zoom, motion system (demo)

---
+++ src/assets/canvasSetup.js
@@ -0,0 +1,55 @@
+//@ts-check
+"use strict";
+
+
+/**
+ * Draw tiles
+ * @param {HTMLCanvasElement} canvas 
+ * @param {CanvasRenderingContext2D} ctx
+ * @param {number} xrange 
+ * @param {number} yrange 
+ * @param {number} tile_width 
+ * @param {number} tile_height 
+ * @param {string} src_formatter 
+ * @param {Function} callback
+ * @returns {Promise<any>}
+ *   callbacked promise
+ */
+async function drawMap(canvas, ctx, xrange, yrange, tile_width, tile_height, src_formatter, callback){
+    /**@type {HTMLImageElement[]} */
+    var al = [];
+    return new Promise((resolve, reject) => {
+        for (var y = 0; y <= yrange; y++){
+            for (var x=0; x <= xrange; x++){
+                var dh = tile_width,
+                    dw = tile_height,
+                    dx = dw*x,
+                    dy = dh*y;
+                !function(x, y, dx, dy, dw, dh){
+                    var img = new Image();
+                    img.onload = function(){
+                        //@ts-ignore
+                        this.loaded = true;
+                        //@ts-ignore
+                        bctx.drawImage(img, 0, 0, tile_width, tile_height, dx, dy, dw, dh);
+                        //@ts-ignore
+                        ctx.drawImage(backcanvas, ...[ backcanvas.canvas.coords.x ,backcanvas.canvas.coords.y ]);
+                        al.push(img);
+                        if (al.length >= (xrange+1)*(yrange+1))
+                            resolve("canvas loaded");
+                    }
+                    //@ts-ignore
+                    img.src = formatString(src_formatter, y, x);
+                    return 0;
+                }(x, y, dx, dy, dw, dh);
+            }
+        }
+    }).then(() => {
+        if (typeof callback === "function")
+            callback(al);
+    }).then(() => {
+        window.scroll({ top: 0, behavior: "instant" });
+    });
+}
+
+
---
+++ src/assets/shishiji-dts/motion.d.ts
@@ -0,0 +1,85 @@
+interface Position {
+    0: number | null,
+    1: number | null,
+    map: Function,
+    some: Function,
+}
+
+interface NonnullPosition {
+    0: number,
+    1: number,
+    map: Function,
+    some: Function,
+}
+
+interface Coords {
+    x: number,
+    y: number
+}
+
+interface Distance { 
+    x: number, 
+    y: number, 
+    distance: number 
+}
+
+interface touchINFO {
+    touches: Coords[],
+    middle: Coords,
+}
+
+
+type Radian = number;
+type Degree = number;
+
+
+/**
+ * assign when interacted
+ * pointerPosition: temp variable to get previous controler pos (get diff)
+ * cursorPosition: current mouse cursor position (zoom origin)
+*/
+declare var pointerPosition: Position;
+declare var cursorPosition: Position;
+
+
+declare var previousTouchDistance: Distance;
+declare var prevTouches: TouchList;
+
+
+// any rotation (RADIAN) & info
+declare var rotatedThisTime: Radian;
+declare var totalRotateThisTime: Radian;
+declare var pastRotateMin: boolean;
+
+
+
+declare function formatString(str: string, ...args: any[]): string;
+
+
+interface CanvasAttrs {
+    coords: Coords,
+    width: number,
+    height: number,
+    rotation: Radian
+}
+  
+
+interface BackCanvas extends HTMLCanvasElement {
+    canvas: CanvasAttrs;
+}
+
+
+declare const backcanvas: BackCanvas;
+declare const bctx: CanvasRenderingContext2D;
+
+
+export {
+    BackCanvas,
+    Position,
+    Radian,
+    Degree,
+    NonnullPosition,
+    Distance,
+    Coords,
+    touchINFO
+}
