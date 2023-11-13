e271bb7ea9b4aef74af09d3c16e7a60fb8014826
KANOKIw <KUMANKENw@gmail.com>
2023-11-13 22:59:06 +0900

    respond object data

--- src/server/app.ts
+++ src/server/app.ts
@@ -3,12 +3,14 @@ import http from "http";
 import { Server as SocketIO } from "socket.io";
 import fs from "fs";
 import https from 'https';
+import { Random } from "./random";
+import * as mapObjAPI from "./mapObjs";
 
 
 const port = 25565;
 const app = express();
 const server = http.createServer(app);
-const io = new SocketIO(server);
+const ws = new SocketIO(server);
 const https_cert = {
   	key: "",
   	cert: "",
@@ -18,6 +20,23 @@ const https_cert = {
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("./"));
+
+
+
+ws.on("connection", (socket) => {
+	
+	socket.on("/data/update/map-objects", (data) => {
+		const mapobjects_ = mapObjAPI.getAllObjects(false);
+		socket.emit("/client/update/map-objects", mapobjects_);
+	});
+});
+
+
+
+app.get("/data/map-objects", (req, res) => {
+	const mapobjects_ = mapObjAPI.getAllObjects(false);
+	res.send(mapobjects_);
+});
 
 
 app.get("/", function(req, res){
